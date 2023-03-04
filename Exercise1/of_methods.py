406748ae23db77a1a7bbf0a878b6b586ae706faa
theteleton <it8816@student.uni-lj.si>
2023-03-04 16:02:05 +0100

    kaizen day 2 - lucas_kanade finished

--- Exercise1/of_methods.py
+++ Exercise1/of_methods.py
@@ -1,9 +1,29 @@
 import numpy as np
 import cv2
+from ex1_utils import gausssmooth, gaussderiv
 
-def lucaskanade(im1, im2, N):
-    pass
 
-def hornschunck(im1, im2, n_iters, lmbd):
+def lucas_kanade(im1, im2, N):
+    kernel = np.ones((N, N))
+    I1 = gausssmooth(im1, 1)
+    Ix1, Iy1 = gaussderiv(I1, 1)
+
+    I2 = gausssmooth(im2, 1)
+    Ix2, Iy2 = gaussderiv(I2, 1)
+
+ 
+    Ix = (Ix1 + Ix2) / 2 
+    Iy = (Iy1 + Iy2) / 2 
+    It = I1 - I2
+
+    D = (cv2.filter2D(src=(Ix ** 2), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Iy ** 2), ddepth=-1, kernel=kernel)) - (cv2.filter2D(src=(Ix * Iy), ddepth=-1, kernel=kernel) ** 2)
+ 
+    U = -((cv2.filter2D(src=(Iy ** 2), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Ix * It), ddepth=-1, kernel=kernel)) - (cv2.filter2D(src=(Ix * Iy), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Iy * It), ddepth=-1, kernel=kernel))) / D
+    V = -((cv2.filter2D(src=(Ix ** 2), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Iy * It), ddepth=-1, kernel=kernel)) - (cv2.filter2D(src=(Ix * Iy), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Ix * It), ddepth=-1, kernel=kernel)))/ D
+
+    return U, V
+    
+
+def horn_schunck(im1, im2, n_iters, lmbd):
     pass
 
