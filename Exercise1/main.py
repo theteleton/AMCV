3bce94979e3b3f0327cc923a24b8ff1a8f796d4c
theteleton <it8816@student.uni-lj.si>
2023-03-05 10:49:50 +0100

    homework done

--- Exercise1/main.py
+++ Exercise1/main.py
@@ -2,30 +2,31 @@ import numpy as np
 import cv2
 import matplotlib. pyplot as plt
 from ex1_utils import rotate_image, show_flow
-from of_methods import lucas_kanade, horn_schunck, lucas_kanade_pyramid
-
+from of_methods import lucas_kanade, horn_schunck, lucas_kanade_pyramid, horn_schunck_fast
+"""
 im1 = cv2.imread("./collision/00000001.jpg")
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-im2 = cv2.imread("./collision/00000005.jpg")
+im2 = cv2.imread("./collision/00000080.jpg")
 im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-"""
+
 im1 = cv2.imread("./disparity/cporta_left.png")
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
 im2 = cv2.imread("./disparity/cporta_right.png")
 im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
+"""
 im1 = cv2.imread("./lab2/001.jpg")
 im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
 im2 = cv2.imread("./lab2/010.jpg")
 im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-"""
+
+
 # im1 = np.random.rand(200, 200).astype(np.float32)
 # im2 = im1.copy()
 # im2 = rotate_image(im2, -1)
                    
-U_lk, V_lk = lucas_kanade(im1, im2, 3)
-U_hs, V_hs = lucas_kanade_pyramid(im1, im2, 3)
-#U_hs, V_hs = horn_schunck(im1, im2, 1000, 100)
+U_lk, V_lk = lucas_kanade(im1, im2, 7)
+#U_hs, V_hs = lucas_kanade(im1, im2, 7)
+U_hs, V_hs = horn_schunck(im1, im2, 1000, 0.5)
 fig1, ((ax1_11, ax1_12), (ax1_21, ax1_22)) = plt.subplots(2, 2)
 
 ax1_11.imshow(im1)
@@ -34,7 +35,7 @@ ax1_12.imshow(im2)
 show_flow(U_lk, V_lk, ax1_21, type='angle')
 show_flow(U_lk, V_lk, ax1_22, type='field', set_aspect=True)
 
-fig1.suptitle('Lucas−Kanade Optical Flow')
+fig1.suptitle('LK lab2 N=7')
 fig2, ((ax2_11, ax2_12), (ax2_21, ax2_22)) = plt.subplots(2, 2)
 ax2_11.imshow(im1)
 ax2_12.imshow(im2)
@@ -42,5 +43,5 @@ ax2_12.imshow(im2)
 show_flow(U_hs, V_hs, ax2_21, type='angle')
 show_flow(U_hs, V_hs, ax2_22, type='field', set_aspect=True)
 
-fig2.suptitle('Horn−Schunck Optical Flow')
+fig2.suptitle('LK pyramid lab2 N=3')
 plt.show()
--- Exercise1/of_methods.py
+++ Exercise1/of_methods.py
@@ -22,6 +22,12 @@ def lucas_kanade(im1, im2, N):
     U = -((cv2.filter2D(src=(Iy ** 2), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Ix * It), ddepth=-1, kernel=kernel)) - (cv2.filter2D(src=(Ix * Iy), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Iy * It), ddepth=-1, kernel=kernel))) / D
     V = -((cv2.filter2D(src=(Ix ** 2), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Iy * It), ddepth=-1, kernel=kernel)) - (cv2.filter2D(src=(Ix * Iy), ddepth=-1, kernel=kernel) * cv2.filter2D(src=(Ix * It), ddepth=-1, kernel=kernel)))/ D
 
+    for i in range(U.shape[0]):
+        for j in range(V.shape[1]):
+            if np.isnan(U[i][j]):
+                U[i][j] = 0
+            if np.isnan(V[i][j]):
+                V[i][j] = 0
     return U, V
     
 def lucas_kanade_pyramid(im1, im2, N):
@@ -30,6 +36,7 @@ def lucas_kanade_pyramid(im1, im2, N):
 
     normal = (width, height)
     U, V = lucas_kanade(im1, im2, N)
+
     while width >= 20 and height >= 20: 
         im1 = gausssmooth(cv2.resize(src=im1, dsize=(width // 2, height // 2), interpolation = cv2.INTER_AREA), 2)
         im2 = gausssmooth(cv2.resize(src=im2, dsize=(width // 2, height // 2), interpolation = cv2.INTER_AREA), 2)
@@ -40,6 +47,12 @@ def lucas_kanade_pyramid(im1, im2, N):
         U1, V1 = lucas_kanade(im1, im2, N)
         U = U + cv2.resize(src=U1, dsize=normal, interpolation = cv2.INTER_AREA)
         V = V + cv2.resize(src=V1, dsize=normal, interpolation = cv2.INTER_AREA)
+        for i in range(U.shape[0]):
+            for j in range(V.shape[1]):
+                if np.isnan(U[i][j]):
+                    U[i][j] = 0
+                if np.isnan(V[i][j]):
+                    V[i][j] = 0
 
     return U, V
 
@@ -56,6 +69,46 @@ def horn_schunck(im1, im2, n_iters, lmbd):
 
     U = np.zeros(im1.shape)
     V = np.zeros(im2.shape)
+
+    Ix = (Ix1 + Ix2) / 2 
+    Iy = (Iy1 + Iy2) / 2 
+    It = I1 - I2
+    It = gausssmooth(It, 1)
+    i = 0
+    while i <= n_iters:
+        Ua = cv2.filter2D(src=U, ddepth=-1, kernel=Ld)
+        Va = cv2.filter2D(src=V, ddepth=-1, kernel=Ld)
+
+        P = (Ix * Ua) + (Iy * Va) + It
+        D = lmbd + (Ix ** 2) + (Iy ** 2)
+ 
+        U = Ua - (Ix * P / D)
+        V = Va - (Iy * P / D)
+        i += 1
+    
+    return U, V
+
+
+def horn_schunck_fast(im1, im2, n_iters, lmbd, N):
+    # kernel = np.ones((N, N))
+    Ld = np.array([[0, 1/4, 0],
+                    [1/4, 0, 1/4],
+                    [0, 1/4, 0]])
+    I1 = im1
+    Ix1, Iy1 = gaussderiv(I1, 1)
+
+    I2 = im2
+    Ix2, Iy2 = gaussderiv(I2, 1)
+
+    U, V = lucas_kanade(im1, im2, N)
+
+    for i in range(U.shape[0]):
+        for j in range(V.shape[1]):
+            if np.isnan(U[i][j]):
+                U[i][j] = 0
+            if np.isnan(V[i][j]):
+                V[i][j] = 0
+            
 
     Ix = (Ix1 + Ix2) / 2 
     Iy = (Iy1 + Iy2) / 2 
