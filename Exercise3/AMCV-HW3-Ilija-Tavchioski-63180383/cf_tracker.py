675580c9033ad67f4d5038af06e9ba3cc884c08b
theteleton <it8816@student.uni-lj.si>
2023-04-29 08:33:56 +0200

    sadfdf

---
+++ Exercise3/AMCV-HW3-Ilija-Tavchioski-63180383/cf_tracker.py
@@ -0,0 +1,80 @@
+import numpy as np
+import cv2
+import time
+from ex2_utils import get_patch, extract_histogram, create_epanechnik_kernel, backproject_histogram
+from ex3_utils import create_gauss_peak, create_cosine_window
+from utils.tracker import Tracker
+
+
+class CorrelationFilterTracker(Tracker): 
+
+    def __init__(self):
+        super().__init__()
+        self.parameters = CFParams()
+
+    def name(self):
+        return "cf15-"
+    
+    def initialize(self, image, region):
+        #print(region) 
+        #start = time.time()
+        self.region = region
+        self.position = (region[0] + (region[2] // 2), region[1] + (region[3] // 2))
+        self.size = (int(((region[2] * self.parameters.increasing_factor) // 2 * 2) + 1), int(((region[3] * self.parameters.increasing_factor) // 2 * 2) + 1))
+        self.patch, mask = get_patch(image, self.position, self.size)
+        self.cosine_filter = create_cosine_window((self.size[0], self.size[1]))
+        self.gauss_filter = create_gauss_peak(self.size, self.parameters.sigma)
+        self.gauss_filter_fft = np.fft.fft2(self.gauss_filter)
+        self.patch_gray = cv2.cvtColor(self.patch, cv2.COLOR_BGR2GRAY)
+        self.f_hat = np.fft.fft2(self.patch_gray * self.cosine_filter)
+        self.f_hat_conjugate = np.conj(self.f_hat)
+        self.h_hat_conj = (self.gauss_filter_fft * self.f_hat_conjugate) / (self.f_hat * self.f_hat_conjugate + 0.1)
+        #end = time.time()
+        #print(start - end)
+
+
+    def track(self, image):
+        #start = time.time()
+        patch, mask = get_patch(image, self.position, self.size)
+        patch_gray = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
+        f_hat = np.fft.fft2(patch_gray * self.cosine_filter)
+
+        r = np.fft.ifft2(self.h_hat_conj * f_hat)
+        r = np.real(r)
+
+        (dy, dx) = np.unravel_index(np.argmax(r, axis=None), r.shape)
+
+    
+        (x, y) = self.position
+        if dx > self.size[0] // 2:
+            dx = dx - self.size[0]
+
+        if dy > self.size[1] // 2:
+            dy = dy - self.size[1]
+
+        x_new = x + dx
+        y_new = y + dy
+
+        self.position = (x_new, y_new)
+
+        patch, mask = get_patch(image, self.position, self.size)
+        patch_gray = cv2.cvtColor(patch, cv2.COLOR_BGR2GRAY)
+        f_hat = np.fft.fft2(patch_gray * self.cosine_filter)
+        f_hat_conjugate = np.conj(f_hat)
+        h_hat_conj_new = (self.gauss_filter_fft * f_hat_conjugate) / (f_hat * f_hat_conjugate + 0.1)
+
+        self.h_hat_conj = ((1 - self.parameters.alpha) * self.h_hat_conj) + (self.parameters.alpha * h_hat_conj_new)
+        #end = time.time();
+        #print(end - start)
+        if self.parameters.increasing_factor != 1:
+            return [self.position[0] - self.region[2] // 2, self.position[1] - self.region[3] // 2, self.region[2], self.region[3]]
+        return [self.position[0] - self.size[0] // 2, self.position[1] - self.size[1] // 2, self.size[0], self.size[1]]
+    
+
+
+ 
+class CFParams():
+    def __init__(self):
+        self.sigma = 2
+        self.alpha = 0.1
+        self.increasing_factor = 1.5
